160dfac9e137d279e813a675edc4de79e69f7d2e
CalebAltuna <web24@cvalls@imaltuna.com>
2026-01-24 17:13:37 +0100

    solving gym to odoo debug

---
+++ odoo/addons/gym_kudeaketa/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Gym Kudeaketa',
+    'version': '1.0',
+    'summary':'Gym kudeaketa sistema',
+    'description': 'Gym kudeaketa sistema',
+    'author':'Caleb',
+    'category': 'Tools',
+    'depends':['base'],
+    'data': [
+            'security/ir.model.access.csv',
+            # groups badaude xml-a inportatu egin behar da
+            'security/groups.xml',
+            # ordena eragina du.
+            'views/gym_bazkideak_views.xml',
+            ],
+    'installable': True,
+    'application':True,
+}
+
