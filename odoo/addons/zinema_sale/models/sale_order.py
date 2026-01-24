e06f6b9e5aad72c250227fc109108ece868b741a
CalebAltuna <web24@cvalls@imaltuna.com>
2026-01-24 23:17:04 +0100

    pelikula id? wft

--- odoo/addons/zinema_sale/models/sale_order.py
+++ odoo/addons/zinema_sale/models/sale_order.py
@@ -4,4 +4,4 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order.line'
 
-    pelikula_id = fields.Many2one('zinea.pelikula', string='Pelikula')
+    member_id = fields.Many2one('zinea.pelikula', string='Pelikula')
