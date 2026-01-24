9c27b352e7e5a3ff3345a1a21a40e02d625c2df6
CalebAltuna <web24@cvalls@imaltuna.com>
2026-01-24 17:02:07 +0100

    prueba gym

---
+++ odoo/addons/zinema_kudeaketa copy/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Gym Kudeaketa',
+    'version': '1.0',
+    'summary':'Gym kudeaketa',
+    'description': 'Gym kudeaketa ahalbidetzen du modulo hau',
+    'author':'Caleb',
+    'category': 'Tools',
+    'depends':['base'],
+    'data': [
+            'security/ir.model.access.csv',
+            'views/pelikula_views.xml',
+            ],
+    'installable': True,
+    'application':True,
+}
