d12ff2fd841706db2f31ef5da3141376b41db0c9
caastiii <cr8151203@gmail.com>
2026-01-27 12:44:35 +0100

    otrocambio

--- laravel/app/Models/gastuak.php
+++ laravel/app/Models/gastuak.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class gastuak extends Model
-{
-    //
+class Gastuak extends Model{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'izena',
+        'deskribapena',
+        'gastu_mota',
+        'user_erosle_id',
+        'user_partaide_id',
+        'totala'
+    ];
 }
