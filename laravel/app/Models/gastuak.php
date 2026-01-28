aa5e4efba5746d9d956fadd121932f6e48acadeb
CalebAltuna <web24@cvalls@imaltuna.com>
2026-01-28 08:21:53 +0100

    subiendo cambios en clases

--- laravel/app/Models/gastuak.php
+++ laravel/app/Models/gastuak.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class gastuak extends Model
 {
     //
+        protected $fillable = [
+        'odoo_id',
+        'synced',
+        'sync_error',
+    ];
 }
