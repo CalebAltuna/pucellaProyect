235807f7f571e2fe10e6b7cd70d55fd3e4c23ab2
CalebAltuna <web24@cvalls@imaltuna.com>
2026-01-28 08:22:58 +0100

    cambio gastuak

--- laravel/app/Models/gastuak.php
+++ laravel/app/Models/gastuak.php
@@ -8,6 +8,13 @@ class gastuak extends Model
 {
     //
         protected $fillable = [
+        'id',
+        'izena',
+        'deskribapena',
+        'gastu_mota',
+        'user_erosle_id',
+        'user_partaide_id',
+        'totala',
         'odoo_id',
         'synced',
         'sync_error',
