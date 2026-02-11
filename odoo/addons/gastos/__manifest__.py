4e0fcfaae037b3ed8597e64732eea1757abf6877
CalebAltuna <web24@cvalls@imaltuna.com>
2026-02-11 12:46:25 +0100

    cambios en files

--- odoo/addons/gastos/__manifest__.py
+++ odoo/addons/gastos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Gastos Pisos',
-    'depends': ['hr_expense', 'hr', 'pisua'],
+    'depends': ['base', 'hr_expense', 'hr', 'pisua'],
     'data': [
         'views/expense_view_inherit.xml',
     ],
