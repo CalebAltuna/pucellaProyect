a23f9e9dd4a0cd9398459a81994b19bd7f2484be
CalebAltuna <web24@cvalls@imaltuna.com>
2026-02-11 08:18:17 +0100

    odoo backup

--- odoo/addons/pisua/models/pisua.py
+++ odoo/addons/pisua/models/pisua.py
@@ -2,15 +2,11 @@ from odoo import models, fields
 
 class Pisua(models.Model):
     _name = 'pisua.pisua'
-    _inherit = ['mail.thread', 'mail.activity.mixin'] 
-    _description = 'Gestión de Pisos'
-
+    _description = 'Gestión de Pisos e Inmuebles'
     name = fields.Char(string='Nombre del Piso', required=True)
     code = fields.Char(string='Código Referencia')
-    coordinator_id = fields.Many2one('res.users', string='Coordinador')
-
-    expense_ids = fields.One2many(
-        'hr.expense', 
-        'pisua_id', 
-        string='Gastos del Piso'
+    coordinator_id = fields.Many2one(
+        comodel_name='res.users', 
+        string='Coordinador',
+        help="Usuario responsable de este piso"
     )
